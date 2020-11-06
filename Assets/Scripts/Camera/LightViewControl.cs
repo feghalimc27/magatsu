f57c40f0fa8e0318dcde455692246e9f83f44b92
mibikin <mibikin@gmail.com>
2020-11-06 14:54:36 -0700

    Add foundation for luminescence with background change

---
+++ Assets/Scripts/Camera/LightViewControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightViewControl : MonoBehaviour {
+
+    public float minLight;
+    public float maxLight;
+
+    [SerializeField]
+    private float currentLight;
+
+    public Texture2D viewImage;
+
+    private Camera camera;
+
+    // Start is called before the first frame update
+    void Start() {
+        camera = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float H, S, V;
+
+        Color.RGBToHSV(camera.backgroundColor, out H, out S, out V);
+
+        camera.backgroundColor = Color.HSVToRGB(H, S, GetCurrentBrightness());
+
+        if (currentLight > maxLight) {
+            currentLight = maxLight;
+        }
+        else if (currentLight < minLight) {
+            currentLight = minLight;
+        }
+    }
+
+    float GetCurrentBrightness() {
+        return 0.2f + currentLight / maxLight;
+    }
+}
