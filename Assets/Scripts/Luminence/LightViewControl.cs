ab4e779434b6e4d227d97cdd9c4c2c04bcec4fea
Mario Feghali <mibikin@gmail.com>
2021-01-12 15:49:00 -0700

    Added player movement and more lighting changes

--- Assets/Scripts/Luminence/LightViewControl.cs
+++ Assets/Scripts/Luminence/LightViewControl.cs
@@ -7,25 +7,26 @@ public class LightViewControl : MonoBehaviour {
     public float minLight;
     public float maxLight;
 
+    private float _minLight = 1;
+    private float _maxLight = 5;
+
     [SerializeField]
     private float currentLight;
 
     public Texture2D viewImage;
 
-    private Camera camera;
-
-    // Start is called before the first frame update
-    void Start() {
-        camera = gameObject.GetComponent<Camera>();
-    }
+    [SerializeField]
+    private Camera cameraRef;
+    [SerializeField]
+    private Light spotLight;
 
     // Update is called once per frame
     void Update() {
         float H, S, V;
 
-        Color.RGBToHSV(camera.backgroundColor, out H, out S, out V);
+        Color.RGBToHSV(cameraRef.backgroundColor, out H, out S, out V);
 
-        camera.backgroundColor = Color.HSVToRGB(H, S, GetCurrentBrightness());
+        cameraRef.backgroundColor = Color.HSVToRGB(H, S, GetCurrentBrightness());
 
         if (currentLight > maxLight) {
             currentLight = maxLight;
@@ -33,6 +34,12 @@ public class LightViewControl : MonoBehaviour {
         else if (currentLight < minLight) {
             currentLight = minLight;
         }
+
+        spotLight.intensity = GetLightIntensity();
+    }
+
+    float GetLightIntensity() {
+        return Mathf.Clamp(_maxLight * (currentLight / maxLight), _minLight, _maxLight);
     }
 
     float GetCurrentBrightness() {
---
+++ Assets/Scripts/Player/playerMovement.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    // Vertical Mobility
+    public float fullHopVelocity;
+    public float doubleJumpVelocity;
+    public float fallSpeed;
+    public float gravity;
+
+    // Horizontal Mobility
+    public float groundAcceleration;
+    public float airAcceleration;
+    public float maxRunSpeed;
+    public float airSpeed;
+    public float friction;
+    public float airFriction;
+
+    private Rigidbody2D rb;
+    private Vector2 movementVector;
+
+    // Movement Flags
+    // DIRECTION: true facing right, false facing left
+    private bool direction = true;
+    private bool grounded = false;
+    private bool hasDJ = true;
+
+    private bool jumpButtonPressed = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        movementVector = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        ApplyMovement();
+        MovementControls();
+        ApplyFriction();
+        CutHorizontalSpeed();
+
+        ApplyGravity();
+        Jump();
+    }
+
+    void MovementControls() {
+        if (Mathf.Abs(movementVector.x) < maxRunSpeed && grounded) {
+            movementVector.x += groundAcceleration * Input.GetAxis("Horizontal");
+        }
+        if(Mathf.Abs(movementVector.x) < airSpeed && !grounded) {
+            movementVector.x += airAcceleration * Input.GetAxis("Horizontal");
+        }
+    }
+
+    void Jump() {
+        if (Input.GetAxis("Jump") > 0 && !jumpButtonPressed) {
+            jumpButtonPressed = true;
+            if (grounded) {
+                movementVector.y = fullHopVelocity;
+                grounded = false;
+            }
+            else if (hasDJ) {
+                movementVector.y = doubleJumpVelocity;
+                hasDJ = false;
+            }
+            else {
+                // Do nothing
+            }
+        }
+        
+        if (Input.GetAxis("Jump") == 0) {
+            jumpButtonPressed = false;
+        }
+    }
+
+    void ApplyFriction() {
+        if (grounded) {
+            movementVector.x *= friction;
+        }
+        else {
+            movementVector.x *= airFriction;
+        }
+    }
+
+    void ApplyGravity() {
+        if (movementVector.y > -fallSpeed && !grounded) {
+            movementVector.y -= gravity;
+        }
+
+        if (movementVector.y < -fallSpeed && !grounded) {
+            movementVector.y = -fallSpeed;
+        }
+    }
+
+    void CutHorizontalSpeed() {
+        if (Mathf.Abs(rb.velocity.x) < 0.05 && Input.GetAxis("Horizontal") == 0) {
+            rb.velocity = new Vector2(0, movementVector.y);
+        }
+    }
+
+    void ApplyMovement() {
+        rb.velocity = movementVector;
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            grounded = true;
+            hasDJ = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            grounded = false;
+        }
+    }
+}
